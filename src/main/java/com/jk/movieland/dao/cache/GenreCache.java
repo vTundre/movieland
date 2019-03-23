9083d73f8f766fff9912a058094127b73e48ab8d
vTundre <jula.kora@gmail.com>
2019-03-24 00:13:29 +0200

    [t-1] create genre cache

---
+++ src/main/java/com/jk/movieland/dao/cache/GenreCache.java
@@ -0,0 +1,37 @@
+package com.jk.movieland.dao.cache;
+
+import com.jk.movieland.dao.GenreDao;
+import com.jk.movieland.entity.Genre;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Primary
+public class GenreCache implements GenreDao{
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    private GenreDao genreDao;
+    private volatile List<Genre> cachedGenres;
+
+    @Autowired
+    public GenreCache(GenreDao genreDao) {
+        this.genreDao = genreDao;
+    }
+
+    @Scheduled(fixedDelayString  = "${cash.genreRefresh}")
+    public void refreshCache() {
+        log.debug("-------------CACHE REFRESH");
+        cachedGenres = genreDao.findAll();
+    }
+
+    @Override
+    public List<Genre> findAll() {
+        return new ArrayList<>(cachedGenres);
+    }
+}
---
+++ src/test/java/com/jk/movieland/dao/jdbc/cache/GenreCacheTest.java
@@ -0,0 +1,35 @@
+package com.jk.movieland.dao.jdbc.cache;
+
+import com.jk.movieland.dao.GenreDao;
+import com.jk.movieland.dao.cache.GenreCache;
+import com.jk.movieland.entity.Genre;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.mockito.Mockito.*;
+
+public class GenreCacheTest {
+    @Test
+    public void testFindAll() {
+        GenreDao genreDao = mock(GenreDao.class);
+        GenreCache genreCache = new GenreCache(genreDao);
+
+        Genre first = new Genre();
+        first.setId(1);
+        first.setName("Ужасы");
+
+        Genre second = new Genre();
+        second.setId(2);
+        second.setName("Фантастика");
+
+        when(genreDao.findAll()).thenReturn(Arrays.asList(first, second));
+
+        genreCache.refreshCache();
+        genreCache.findAll();
+        genreCache.findAll();
+
+        verify(genreDao, times(1)).findAll();
+        verifyNoMoreInteractions(genreDao);
+    }
+}
